84f2000c0d2dcdc1bc107ed25754291d1631e3b4
Kevin Bueno <kean.solano@gmail.com>
2022-02-04 22:21:53 -0500

    admin courses integration

--- src/app/features/teacher/components/create-course/create-course.component.ts
+++ src/app/features/teacher/components/create-course/create-course.component.ts
@@ -33,10 +33,10 @@ export class CreateCourseComponent implements OnInit {
 
     this.courseForm = this.formBuilder.group({
       title: new FormControl('', Validators.compose([
-        Validators.required,
+        Validators.required, Validators.minLength(8), Validators.maxLength(100)
       ])),
       description: new FormControl('', Validators.compose([
-        Validators.required, Validators.minLength(8), Validators.maxLength(100)
+        Validators.required, Validators.minLength(8), Validators.maxLength(500)
       ])),
       price: new FormControl('', Validators.compose([
         Validators.required, Validators.min(1)
@@ -57,7 +57,6 @@ export class CreateCourseComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
   }
 
 
--- src/app/features/teacher/components/create-topics/create-topics.component.ts
+++ src/app/features/teacher/components/create-topics/create-topics.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { UtilsService } from '../../../../core/services/utils.service';
+import { CoursesService } from '../../../../shared/services/courses.service';
+import { NotificationsService } from '../../../../core/services/notifications.service';
 
 @Component({
   selector: 'app-create-topics',
@@ -7,9 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateTopicsComponent implements OnInit {
 
-  constructor() { }
+  modules = [];
+
+  topicForm: FormGroup;
+  validationMessages: any;
+
+  errorMessage: string | null;
+
+
+  constructor(
+    private utilsService: UtilsService,
+    private formBuilder: FormBuilder,
+    private coursesService: CoursesService,
+    private notificationsService: NotificationsService
+  ) {
+
+    this.validationMessages = utilsService.getValidationMessages();
+
+    this.topicForm = this.formBuilder.group({
+      course: new FormControl('', Validators.compose([
+        Validators.required,
+      ])),
+      title: new FormControl('', Validators.compose([
+        Validators.required,
+      ])),
+      description: new FormControl('', Validators.compose([
+        Validators.required, Validators.minLength(8), Validators.maxLength(100)
+      ])),
+      video: new FormControl('', Validators.compose([
+      ])),
+      files: new FormControl('', Validators.compose([
+      ]))
+    });
+
+  }
+
+  createTopic(dataForm: any) {
+    console.log(dataForm);
+  }
 
   ngOnInit(): void {
   }
+
 
 }
