dda5124ee0cc3bb9fa27c16ba9b1a88ef03a77ad
Jhonanderson Tellez <tellezjhonander@gmail.com>
2023-04-03 20:19:39 -0500

    Add package validation and conditional rendering based on its presence

--- src/app/features/trading-area/pages/trading-area/trading-area.component.ts
+++ src/app/features/trading-area/pages/trading-area/trading-area.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PacksService } from '../../services/packs.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-trading-area',
   templateUrl: './trading-area.component.html',
   styleUrls: ['./trading-area.component.scss']
 })
-export class TradingAreaComponent {
+export class TradingAreaComponent implements OnInit {
 
+  constructor(
+    private packsService: PacksService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+      this.packsService.getMyPacks().subscribe(
+        response => {
+          if (response.length > 0) {
+            this.router.navigate(['trading-area/my-packages']);
+          }
+        },
+        error => {
+          console.log(error);
+        }
+      )
+  }
 }
