1d897c060da1856f412513d88e7d6432ecfc243b
Kevin Bueno <kean.solano@gmail.com>
2023-04-25 09:26:20 +1000

    CREATED customer service module

--- src/app/features/customer-service/customer-service-routing.module.ts
+++ src/app/features/customer-service/customer-service-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerServiceComponent } from './customer-service.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: CustomerServiceComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
