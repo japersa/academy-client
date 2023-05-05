24fa37e56b1b93f0d7d441d922fbcbef438708c0
Jhonanderson Tellez <tellezjhonander@gmail.com>
2023-05-05 13:55:07 -0500

    Packegage demo form

--- src/app/features/funding-program/components/create-demo/create-demo.component.ts
+++ src/app/features/funding-program/components/create-demo/create-demo.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UtilsService } from 'src/app/core/services/utils.service';
+import { PacksService } from '../../services/packs.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-demo',
@@ -7,4 +11,35 @@ import { Component } from '@angular/core';
 })
 export class CreateDemoComponent {
 
+  price = '';
+  form!: FormGroup;
+  errorMessage: string | null;
+/*   newPack: any [] = null; */
+
+  constructor(
+    // import the form builder
+    private formBuilder: FormBuilder,
+    public utilsService: UtilsService,
+    private packsService: PacksService,
+    private router: Router
+  ) {
+    // Build the form
+    this.buildForm();
+  }
+
+  // create the form
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      balance: ['', [Validators.required]],
+      currencies: ['', [Validators.required]],
+      account_type: ['', [Validators.required]],
+    });
+  }
+
+  createPack(formData: any) {
+    this.packsService.createPackDemo(formData).subscribe( pkg => {
+      console.log(pkg);
+    } );
+  }
 }
+ 
