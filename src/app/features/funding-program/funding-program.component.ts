8a6ac1bbf47bbf59d75481cc6b89006f9029c0c7
Kevin Bueno <kean.solano@gmail.com>
2023-05-21 14:45:20 +1000

    FIXED demo

--- src/app/features/funding-program/funding-program.component.ts
+++ src/app/features/funding-program/funding-program.component.ts
@@ -3,7 +3,7 @@ import { PacksService } from './services/packs.service';
 import { Router } from '@angular/router';
 import { UserDataService } from 'src/app/core/services/user-data.service';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
- 
+
 @Component({
   selector: 'app-trading-area',
   templateUrl: './funding-program.component.html',
@@ -11,15 +11,15 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 })
 export class FundingProgramComponent implements OnInit {
 
-  referalCode = '0000000'; 
-  loginDemo = '0000000'; 
+  referalCode = '0000000';
+  loginDemo = '0000000';
   passDemo = '0000000';
-  invesPassDemo = '0000000'; 
+  invesPassDemo = '0000000';
   buttonTextLogin = 'Copiar';
   buttonTextPass = 'Copiar';
   buttonTextPassInves = 'Copiar';
 
- 
+
   myPackages: any[] = [];
   packDemo: any;
 
@@ -34,18 +34,18 @@ export class FundingProgramComponent implements OnInit {
 
   copied(button: string) {
 
-    if (button == 'login'){
+    if (button == 'login') {
       this.buttonTextLogin = 'Copiado';
-    }else if (button == 'pass'){
+    } else if (button == 'pass') {
       this.buttonTextPass = 'Copiado';
-    }else if (button == 'passInves'){
+    } else if (button == 'passInves') {
       this.buttonTextPassInves = 'Copiado';
     }
-    
+
   }
 
   getMypackages() {
-    this.packsService.getMyPacks().subscribe( 
+    this.packsService.getMyPacks().subscribe(
       response => {
         this.myPackages = response;
       },
@@ -55,54 +55,47 @@ export class FundingProgramComponent implements OnInit {
     );
   }
 
-  getDemo(){
+  getDemo() {
     this.packsService.getPacksDemo().subscribe(
-       pkgDemo => {
-          console.log(pkgDemo);
-        }, 
-        error  => {
-          console.log(error);
-        }
-    
+      pkgDemo => {
+        console.log(pkgDemo);
+      },
+      error => {
+        console.log(error);
+      }
+
     )
   }
 
-  show(template: TemplateRef<any>){
+  show(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
   }
 
   ngOnInit(): void {
-    
+
     this.packsService.getPacksDemo().subscribe(
       pkgDemo => {
-         console.log(pkgDemo);
-       }, 
-       error  => {
-         console.log(error);
-       }
-   )
-  
-    this.packsService.getPackageDemoById('3').subscribe(
-      {
-        next: (pkgDemo) => {
-          this.packDemo = pkgDemo;
-          this.loginDemo = pkgDemo?.mt_login;
-          this.passDemo = pkgDemo?.mt_password;
-          this.invesPassDemo = pkgDemo?.mt_password_investor;
-          console.log(this.packDemo);
-          console.log(this.packDemo.length);
-        },
-        error: (e) => console.log(e)
+        let demo = pkgDemo[0];
+        console.log(demo);
+        this.packDemo = demo;
+        this.loginDemo = demo?.mt_login;
+        this.passDemo = demo?.mt_password;
+        this.invesPassDemo = demo?.mt_password_investor;
+      },
+      error => {
+        console.log(error);
       }
     )
-    
-    
-    
+
+
+
+
+
     this.getMypackages();
     this.userDataService.userData$.subscribe(
       {
-       next: (r) => this.referalCode = r?.referral_code
-      } 
-     );
+        next: (r) => this.referalCode = r?.referral_code
+      }
+    );
   }
 }
