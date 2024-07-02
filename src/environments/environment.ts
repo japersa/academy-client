086d9e66216185cd8a07fa941fba12e1635a45e5
Luis Higuera <lahiguera@enerbit.co>
2024-07-02 14:40:03 -0500

    bbkbkbkb

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   environmentName: 'Development',
-  apiURL: 'https://ultra-backend-c8f5a527241c.herokuapp.com',
+  apiURL: 'https://ultra-back-98fc6bf42dce.herokuapp.com',
   rollbarConfig: {
     accessToken: '748115ffc5b04e71ba4d73da8ca51159',
     captureUncaught: true,
