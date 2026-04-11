2127721b0a581cd99cb10b75452c38178824d65c
japersa <japersa92@gmail.com>
2026-04-10 21:11:26 -0500

    fix(user): update API endpoint for user profile retrieval
    - Modified the user service to append a trailing slash to the API URL for fetching the user profile.
    - This change ensures consistency in API requests and aligns with backend expectations.
    
    This update improves the reliability of user profile retrieval by standardizing the API endpoint format.

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -12,7 +12,7 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   getUser() {
-    return this.http.get<any>(`${apiURL}/profile/me`);
+    return this.http.get<any>(`${apiURL}/profile/me/`);
   }
 }
 
